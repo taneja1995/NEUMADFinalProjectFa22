fcc9f0240520633fcd8fcd8114a7b38b7d4947be
PriyaRaul <raul.priya6@gmail.com>
2022-12-04 11:51:58 -0800

    Implementation of increment and decrement of food quantity

--- app/src/main/java/com/example/foodorderingapp/MenuListActivity.java
+++ app/src/main/java/com/example/foodorderingapp/MenuListActivity.java
@@ -4,9 +4,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,11 +22,15 @@ import java.util.List;
 public class MenuListActivity extends AppCompatActivity {
 
     TextView noOfItem;
-    private List<OrderedItem> menuList = new ArrayList<OrderedItem>();
+    private List<OrderedItem> menuLists = new ArrayList<OrderedItem>();
 
     OrderedItem orderedItem;
     int count=0;
     RecyclerView menuListRV;
+    FirebaseDatabase firebaseDatabase;
+    private DatabaseReference reference;
+    FirebaseStorage firebaseStorage;
+    MenuListAdapter menuListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +38,59 @@ public class MenuListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_foodmenu);
 
         noOfItem= (TextView) findViewById(R.id.noOfItem);
-        this.menuList.add(new OrderedItem("rice",""));
-        this.menuList.add(new OrderedItem("roti",""));
-        this.menuList.add(new OrderedItem("bread",""));
+        //this.menuLists.add(new OrderedItem("rice",""));
+       // this.menuList.add(new OrderedItem("roti",""));
+        //this.menuList.add(new OrderedItem("bread",""));
+
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        reference = firebaseDatabase.getReference().child("Restaurant");
+        firebaseStorage = FirebaseStorage.getInstance();
+        reference.keepSynced(true);
+        menuListAdapter = new MenuListAdapter( menuLists,this);
+
+        addDataItem();
+        DisplayRecyclerView();
+    }
+
+    private void DisplayRecyclerView() {
         menuListRV = findViewById(R.id.menuListRV);
-
         menuListRV.setLayoutManager(new LinearLayoutManager(this));
+        menuListRV.setAdapter(menuListAdapter);
+    }
 
-        menuListRV.setAdapter(new MenuListAdapter(menuList, this));
+    private void addDataItem() {
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                menuLists.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    //OrderedItem = new OrderedItem();
+                    DataSnapshot menuList = snapshot.child("MenuList");
+                    String ResName=snapshot.getKey();
+                    System.out.println("----------------"+ResName);
+                    if(ResName.equals("Chipotle")){
+
+                    for(DataSnapshot menu:menuList.getChildren()) {
+                        OrderedItem orderedItem=new OrderedItem();
+                        orderedItem.setMenuImage(menu.child("Image").getValue().toString());
+                        orderedItem.setOrderedItemName(menu.getKey());
+                        orderedItem.setPrice(menu.child("Price").getValue().toString());
+                        System.out.println("----------------" + menu.getKey() + "--" + menu.child("Price").getValue().toString());
+                        menuLists.add(orderedItem);
+                        menuListAdapter.notifyDataSetChanged();
+                    }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+
+        });
     }
 
    /* public void increment(View v){
--- app/src/main/java/com/example/foodorderingapp/MenuListAdapter.java
+++ app/src/main/java/com/example/foodorderingapp/MenuListAdapter.java
@@ -1,15 +1,22 @@
 package com.example.foodorderingapp;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -17,13 +24,7 @@ public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.MenuLi
     private final List<OrderedItem> orderedItems;
     private final Context context;
     int count=0;
-    Activity activity;
 
-    public MenuListAdapter(List<OrderedItem> orderedItems, Context context, Activity activity) {
-        this.orderedItems = orderedItems;
-        this.context = context;
-        this.activity=activity;
-    }
 
 
     public MenuListAdapter(List<OrderedItem> orderedItems, Context context) {
@@ -33,19 +34,31 @@ public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.MenuLi
 
     @NonNull
     @Override
-    public MenuListAdapter.MenuListHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.single_menu, parent, false);
-        return new MenuListHolder(view);
+    public MenuListHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       // View view = LayoutInflater.from(context).inflate(R.layout.single_menu, parent, false);
+       // return new MenuListHolder(view);
+
+        return new MenuListHolder(LayoutInflater.from(context).inflate(R.layout.single_menu, null));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MenuListAdapter.MenuListHolder holder, int position) {
-        holder.orderedItemName.setText(orderedItems.get(position).getLinkText());
-       // holder.orderedItemQuantity.setText(String.valueOf(orderedItems.get(position).getOrderedItemQuantity()));
+    public void onBindViewHolder(@NonNull MenuListHolder holder, @SuppressLint("RecyclerView") int position) {
+        OrderedItem orderedItem = orderedItems.get(position);
+        System.out.println("//////////"+orderedItems.get(position).getOrderedItemName());
+        holder.orderedItemName.setText(orderedItems.get(position).getOrderedItemName());
+        holder.price.setText(orderedItems.get(position).getPrice());
+        holder.orderedItemQuantity.setText("0");
+        String imageUrl = null;
+
+        imageUrl = orderedItem.getMenuImage();
+        Picasso.get().load(imageUrl).into(holder.foodImage);
         holder.addItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                count++;
+                int ItemCount=0;
+                count=0;
+                ItemCount=Integer.parseInt(holder.orderedItemQuantity.getText().toString());
+                count=ItemCount+1;
                 holder.orderedItemQuantity.setText(""+count);
             }
         });
@@ -53,8 +66,11 @@ public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.MenuLi
         holder.removeItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                int ItemCount=0;
+                count=0;
+                ItemCount=Integer.parseInt(holder.orderedItemQuantity.getText().toString());
                 if(count<0){count=0;}
-                else {count--;}
+                else {count=ItemCount-1;}
                 holder.orderedItemQuantity.setText(""+count);
             }
         });
@@ -68,8 +84,9 @@ public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.MenuLi
     public class MenuListHolder extends RecyclerView.ViewHolder {
 
         public TextView orderedItemName;
-        public TextView orderedItemQuantity;
+        public TextView orderedItemQuantity,price;
         public Button addItem,removeItem;
+        public ImageView foodImage;
 
         public MenuListHolder(@NonNull View itemView) {
             super(itemView);
@@ -77,6 +94,8 @@ public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.MenuLi
             this.orderedItemQuantity=itemView.findViewById(R.id.noOfItem);
             this.addItem=itemView.findViewById(R.id.addItem);
             this.removeItem=itemView.findViewById(R.id.removeItem);
+            this.foodImage=itemView.findViewById(R.id.foodImage);
+            this.price=itemView.findViewById(R.id.menuPrice);
         }
     }
 }
