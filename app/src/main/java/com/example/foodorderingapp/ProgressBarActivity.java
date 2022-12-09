d6be314ba67a8c705bc741f51ffca75d851185d5
taneja1995 <itaneja35@gmail.com>
2022-12-09 02:07:44 -0500

    Programmatically implemented the textview based on status

--- app/src/main/java/com/example/foodorderingapp/ProgressBarActivity.java
+++ app/src/main/java/com/example/foodorderingapp/ProgressBarActivity.java
@@ -1,8 +1,10 @@
 package com.example.foodorderingapp;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +14,7 @@ public class ProgressBarActivity extends AppCompatActivity {
     String orderStatus=null;
     private ProgressBar progressBar;
     private Button btn;
+    private TextView status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,23 +22,25 @@ public class ProgressBarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_progressbar);
         progressBar= findViewById(R.id.progressBar);
         btn= findViewById(R.id.checkStatus_btn);
+        status=findViewById(R.id.status_tv);
         Bundle extras= getIntent().getExtras();
         if(extras!=null){
             orderStatus = extras.getString("status");
-            System.out.println("the order status is"+orderStatus);
         }
         // to show the progress bar status for the order based on the status.
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                System.out.println("inside the progress bar method");
-
                 if(orderStatus.equals("Completed")){
                     progress=100;
                     progressBar.setProgress(progress);
+                    status.setText("Yipeee!! Your order is prepared! Delicious food is on the way!");
+                    status.setTextColor(Color.BLACK);
                 }else{
                     progress=50;
                     progressBar.setProgress(progress);
+                    status.setText("Your order is still in progress! Thanks for your patience!");
+                    status.setTextColor(Color.BLACK);
                 }
             }
         });
