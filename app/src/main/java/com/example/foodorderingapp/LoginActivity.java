9dd69f9db6ce2c38c9eda20de89b82fb8da2a9d3
amirthavarshini <socaklingambama.a@northeastern.edu>
2022-12-10 18:45:24 -0500

    Login activity modification for global state saving of username

--- app/src/main/java/com/example/foodorderingapp/LoginActivity.java
+++ app/src/main/java/com/example/foodorderingapp/LoginActivity.java
@@ -2,19 +2,27 @@ package com.example.foodorderingapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText userName;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        userName = findViewById(R.id.email);
     }
 
     public void showRestaurantList(View view){
+        insertUserData();
+        Log.i("LoginActivity", String.valueOf(userName));
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
@@ -22,5 +30,12 @@ public class LoginActivity extends AppCompatActivity {
     public void signup(View view){
         Intent intent=new Intent(this,SignUpActivity.class);
         startActivity(intent);
+    }
+
+    private void insertUserData() {
+        String user=userName.getText().toString();
+
+        ((MyApplication) this.getApplication()).setUserName(user);
+         // globally set
     }
 }
