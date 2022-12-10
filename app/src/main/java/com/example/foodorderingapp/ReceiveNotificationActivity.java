50863d7fe80d58230045efbeefac9254b0b089f4
amirthavarshini <socaklingambama.a@northeastern.edu>
2022-12-10 18:45:45 -0500

    Notification skeleton

---
+++ app/src/main/java/com/example/foodorderingapp/ReceiveNotificationActivity.java
@@ -0,0 +1,15 @@
+package com.example.foodorderingapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReceiveNotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_notification);
+    }
+
+}
